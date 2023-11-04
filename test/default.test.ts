6eff0cbb377d32ad1be41a1113523093cd1d543c
Jarrod Seccombe <782969+jsec@users.noreply.github.com>
2023-11-04 12:23:05 -0400

    Initial commit

---
+++ test/default.test.ts
@@ -0,0 +1,12 @@
+import test from 'ava';
+import { add } from '../src/index.js';
+
+test('adds two numbers', t => {
+    const result = add(1, 2);
+    t.is(result, 3);
+});
+
+test.failing('should fail', t => {
+    const result = add(1, 2);
+    t.is(result, 4);
+});
